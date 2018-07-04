9a2cb7544331809f98069ab1cdec5f75f0b5ad83
oncampus <education@oncampus.de>
2018-07-04 11:38:01 +0200

    Grundgeruest noch ohne Funktion

---
+++ Loop.i18n.alias.php
@@ -0,0 +1,17 @@
+<?php 
+/**
+ * Aliases for Loop special pages
+ *
+ * @file
+ * @ingroup Extensions
+ */
+
+$specialPageAliases = array();
+
+$specialPageAliases['en'] = array(
+
+	);
+
+$specialPageAliases['de'] = array(
+		
+	);
